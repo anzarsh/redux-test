b1c218079492bc0577e85888abffc878490e9040
Anzar Schumahua <anzarsh@mail.ru>
2022-04-01 11:58:25 +0300

    Initial redux store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {cash}
+      <button
+        type="button"
+        onClick={() => dispatch({ type: 'ADD_CASH', payload: 5 })}
+      >
+        Добавить наличку
+      </button>
+      <button
+        type="button"
+        onClick={() => dispatch({ type: 'GET_CASH', payload: 5 })}
+      >
+        Снять наличку
+      </button>
     </div>
   );
 }
