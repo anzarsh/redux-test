d2bf067f1719616e1ca9400662b23a8eb7e751f3
Anzar Schumahua <anzarsh@mail.ru>
2022-04-01 14:16:20 +0300

    redux-devtools/extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
 import { combineReducers, createStore } from 'redux';
 import { cashReducer } from './cash-reducer';
 import { customerReducer } from './customer-reducer';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-export const store = createStore(combineReducers({
-  cash: cashReducer,
-  customer: customerReducer,
-}));
+export const store = createStore(
+  combineReducers({
+    cash: cashReducer,
+    customer: customerReducer,
+  }),
+  composeWithDevTools(),
+);
