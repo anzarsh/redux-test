8adbbb9e3d84ff656ba466c3e8fc1eeb522e578f
Anzar Schumahua <anzarsh@mail.ru>
2022-04-01 17:04:24 +0300

    redux array actions

--- src/App.js
+++ src/App.js
@@ -3,22 +3,52 @@ import { useDispatch, useSelector } from "react-redux";
 function App() {
   const dispatch = useDispatch();
   const cash = useSelector(state => state.cash.cash);
+  const customers = useSelector(state => state.customers.customers);
+
+  console.log('customers', customers);
+
+  const handleAddCustomer = () => {
+    const newCustomer = {
+      id: Date.now(),
+      name: prompt(),
+    }
+    dispatch({ type: 'ADD_CUSTOMER', payload: newCustomer })
+  }
 
   return (
     <div className="App">
-      {cash}
-      <button
-        type="button"
-        onClick={() => dispatch({ type: 'ADD_CASH', payload: 5 })}
-      >
-        Добавить наличку
-      </button>
-      <button
-        type="button"
-        onClick={() => dispatch({ type: 'GET_CASH', payload: 5 })}
-      >
-        Снять наличку
-      </button>
+      <div>{cash}</div>
+      <div>
+        <button
+          type="button"
+          onClick={() => dispatch({ type: 'ADD_CASH', payload: 5 })}
+        >
+          Добавить наличку
+        </button>
+        <button
+          type="button"
+          onClick={() => dispatch({ type: 'GET_CASH', payload: 5 })}
+        >
+          Снять наличку
+        </button>
+        
+        <button
+          type="button"
+          onClick={handleAddCustomer}
+        >
+          Добавить пользователя
+        </button>
+      </div>
+      <div>
+        {customers.map(c => (
+          <div
+            key={c.id}
+            onClick={() => dispatch({ type: 'REMOVE_CUSTOMER', payload: c.id })}
+          >
+            {c.name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ import { composeWithDevTools } from '@redux-devtools/extension';
 export const store = createStore(
   combineReducers({
     cash: cashReducer,
-    customer: customerReducer,
+    customers: customerReducer,
   }),
   composeWithDevTools(),
 );
